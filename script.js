df18de8f86ee65495c1f7a0b0ed54b432a901722
moshi138 <hasaikaitenten@gmail.com>
2025-05-21 20:22:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function openEnvelope() {
+    document.querySelector('.flap').style.transform = 'rotateX(180deg)';
+    document.querySelector('.message').style.opacity = '1';
+    document.querySelector('.hearts').style.transform = 'translate(-50%, -50%) scale(1)';
+}
